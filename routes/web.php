0636fe9b6be2fd19172cfaba32008af78bad305d
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-05 08:24:06 +0700

    middleware guest

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ use App\Http\Middleware\CheckRole;
 
 // Public routes
 
-    Route::get('/', [AuthController::class, 'showLoginForm']);
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::get('/', [AuthController::class, 'showLoginForm'])->middleware('guest');
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
     Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register')->middleware('guest');
     Route::post('/register', [AuthController::class, 'register'])->name('create.user');
 
 
